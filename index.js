77b89348156e65423a30fd4895bc7fca6fa5de02
Samantha Webb <samantha.webb2436@gmail.com>
2025-02-15 02:29:17 -0600

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(fun1 = "roller-skate"){
+   return `This Saturday, I want to ${fun1}!`;
+};
+
+saturdayFun("bathe my dog");
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`;
+};
+
+mondayWork("work from home");
+
+//
+
+
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
